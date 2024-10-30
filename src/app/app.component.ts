d0b97a48b22e28792653c50828e00d5437690407
Vladimir Apraksin <turbo.headcrab.666@gmail.com>
2024-10-29 22:28:08 -0400

    Add configuration display to AppComponent
    Replaced the title with configuration display in the template. Updated AppComponent to include a configuration signal which is set based on the development mode.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, signal } from '@angular/core';
+import { Component, isDevMode, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
@@ -9,10 +9,18 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  title = 'my-third-static-web-app';
+  configuration = signal<'development' | 'production' | undefined>(undefined);
   count = signal<number>(0);
 
   modifyByOne(isAddition: boolean) {
-    this.count.update((value) => (isAddition ? value + 1 : value - 1));
+    this.count.update((value: number) => (isAddition ? value + 1 : value - 1));
+  }
+
+  constructor() {
+    if (isDevMode()) {
+      this.configuration.set('development');
+    } else {
+      this.configuration.set('production');
+    }
   }
 }
