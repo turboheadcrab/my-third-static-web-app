2efe0a7fc5e20e9b83d2bc9b9403c37ea71589ad
Vladimir Apraksin <turbo.headcrab.666@gmail.com>
2024-10-29 22:49:44 -0400

    Add Navbar component and integrate Angular Material
    Introduced a new Navbar component with associated tests and constants. Updated the main app template and styles to incorporate Angular Material design elements, including cards and buttons. Adjusted package dependencies and configuration files accordingly.

---
+++ src/app/core/navbar/navbar.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import {MatToolbar} from "@angular/material/toolbar";
+import {TITLE} from "./navbar.constant";
+
+@Component({
+  selector: 'app-navbar',
+  standalone: true,
+  imports: [MatToolbar],
+  templateUrl: './navbar.component.html',
+  styleUrl: './navbar.component.scss',
+})
+export class NavbarComponent {
+  protected readonly TITLE = TITLE;
+}
