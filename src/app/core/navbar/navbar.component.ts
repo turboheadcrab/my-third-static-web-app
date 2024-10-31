f151f3e396d33377b425d60000c17c18651100c9
Vladimir Apraksin <turbo.headcrab.666@gmail.com>
2024-10-30 20:56:52 -0400

    Add home button to the navbar
    Introduced a home button to the navbar for improved navigation. Updated navbar.component to include necessary Angular Material modules and styles. Adjusted the template HTML for the new button and added a spacer for layout alignment.

--- src/app/core/navbar/navbar.component.ts
+++ src/app/core/navbar/navbar.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
-import { MatToolbar } from '@angular/material/toolbar';
 import { TITLE } from './navbar.constant';
+import { MatToolbar } from '@angular/material/toolbar';
+import { MatIcon } from '@angular/material/icon';
+import { MatIconButton } from '@angular/material/button';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [MatToolbar],
+  imports: [MatToolbar, MatIcon, MatIconButton, RouterLink],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss',
 })
